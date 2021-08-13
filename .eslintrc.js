719312cd5ce4d6b77bc1381f31d2e9f17ec3c068
Gabriel <14190835+Gzal@users.noreply.github.com>
2021-08-13 08:05:23 -0700

    Add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "browser": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "no-debugger": "warn",
+        "no-unused-vars": "warn", 
+        "no-undef": "warn"
+    } 
+}
